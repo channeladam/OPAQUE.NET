21cd734ca3917142c25d32560d5149ce01fdd362
Adam Craven <adammcraven@gmail.com>
2021-04-14 02:18:28 +1000

    Fix SHA512 code to be compatible with netcoreapp3.1

--- src/Opaque.Net/HashFunctions/Sha512HashFunction.cs
+++ src/Opaque.Net/HashFunctions/Sha512HashFunction.cs
@@ -1,13 +1,16 @@
+using System.Security.Cryptography;
 using Opaque.Net.Abstractions;
 
 namespace Opaque.Net.HashFunctions
 {
     public class Sha512HashFunction : IHashFunction
     {
+        private static readonly SHA512Managed _hashFunction = new();
+
         public ushort InputBlockSizeInBytes => 128;
         public ushort OutputSizeInBits => 512;
         public ushort OutputSizeInBytes => 64;
 
-        public byte[] HashData(byte[] source) => System.Security.Cryptography.SHA512.HashData(source);
+        public byte[] HashData(byte[] source) => _hashFunction.ComputeHash(source);
     }
 }
